9341ea1e60f6374e7586eade30cf72bbdcc3a611
Talvan <talvan.alves00@gmail.com]>
2025-11-11 09:33:26 -0300

    adding iterative mode for collaz sequence in desafioGoogle

--- desfioGoogle/iterativa.cpp
+++ desfioGoogle/iterativa.cpp
@@ -1,34 +1,50 @@
 #include <iostream>
 #include <chrono>
-#include <vector>
 using namespace std;
 
-void collatz_sequence(int n, vector<int>& sequence) {
-    while (n != 1) {
-        sequence.push_back(n);
-        if (n % 2 == 0) {
-            n = n / 2;
-        } else {
-            n = n * 3 + 1;
+long long collatz_sequence(long long n) {
+    long long count = 1;
+    while(n != 1){
+
+        if(n % 2 == 0){
+            n = n /2;
+            
         }
+
+        else{
+            n = 3 * n + 1;
+
+        }    
+        count++;    
     }
-    sequence.push_back(1);
+
+    return count;
 }
 
 int main(){
-    int n;
-    cin >> n;
 
-    chrono::steady_clock::time_point start = chrono::steady_clock::now();
-    for (int i = 0; i < 100000; i++)
-    {
-        vector<int> sequence;
-        collatz_sequence(n, sequence);
-    }
+    long long total_itens = 0;
+    int maior_num = 0;
+
+    auto start = chrono::steady_clock::now();
     
-    chrono::steady_clock::time_point end = chrono::steady_clock::now();
+    for(int i = 1; i < 1000000 ; i++){
 
-    chrono::duration<float> elapsed_seconds = end - start;
-    cout << "\n Time: " << elapsed_seconds.count() << "s\n";
+        long long tamanho = collatz_sequence(i);
+        if (tamanho > total_itens )
+        {
+            total_itens = tamanho;
+            maior_num = i;
+        }
+       
+    }
+    auto end = chrono::steady_clock::now();
+
+    chrono::duration<double> elapsed_seconds = end - start;
+    cout << "Maior sequencia:" << endl;
+    cout << "Numero " << maior_num << " Total de itens: " << total_itens << endl;
+    cout << " Time: " << elapsed_seconds.count() << "s\n";
+
+
     return 0;
 }
